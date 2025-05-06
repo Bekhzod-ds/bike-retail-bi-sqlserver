21971f8f4f3ac84837c69957bedde32152c67651
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-06 10:10:04 +0500

    Update StoreSalesSummary.sql

--- views/StoreSalesSummary.sql
+++ views/StoreSalesSummary.sql
@@ -1,12 +1,12 @@
 CREATE VIEW vw_StoreSalesSummary AS
-select 
+SELECT 
 	store_name, 
 	round(sum((quantity * list_price)*(1-discount)), 2) Revenue, 
-	count(distinct ord.order_id) Orders,
-	round(sum((quantity * list_price)*(1-discount))/count(distinct ord.order_id), 2) AOV
-from Sales.Stores st
-join Sales.Orders ord
-on st.store_id = ord.store_id
-join Sales.Order_items oi
-on ord.order_id = oi.order_id
-group by store_name;
+	COUNT(DISTINCT ord.order_id) Orders,
+	round(sum((quantity * list_price)*(1-discount))/COUNT(DISTINCT ord.order_id), 2) AOV
+FROM Sales.Stores st
+JOIN Sales.Orders ord
+ON st.store_id = ord.store_id
+JOIN Sales.Order_items oi
+ON ord.order_id = oi.order_id
+GROUP BY store_name;
