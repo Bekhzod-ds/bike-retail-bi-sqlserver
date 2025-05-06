0e55fcdf6cafb2595afc8c3f570bf8d9baa78ff4
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-06 22:13:16 +0500

    Update SalesByQuarter(Additional).sql

--- views/SalesByQuarter(Additional).sql
+++ views/SalesByQuarter(Additional).sql
@@ -9,3 +9,5 @@ ON cus.customer_id = ord.customer_id
 JOIN sales.Order_items oi
 ON ord.order_id = oi.order_id
 GROUP BY YEAR(order_date), DATEPART(QUARTER, order_date)
+
+--This view Calculates Total sales by quarter in each year. This allows for tracking trends over a period of time to see how well a company has been performing
