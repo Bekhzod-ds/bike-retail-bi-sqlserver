5e6c32c916950058654c3b957d53ee12361a8999
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-06 10:29:31 +0500

    Create TopBuyingCustomers(Additional).sql

---
+++ views/TopBuyingCustomers(Additional).sql
@@ -0,0 +1,11 @@
+CREATE VIEW vw_TopBuyingCustomers AS
+SELECT TOP 100
+	CONCAT(first_name, ' ', last_name) AS Full_Name,
+	SUM((quantity * list_price) * (1-discount)) AS TotalSales
+FROM Sales.Customers cus
+JOIN sales.Orders ord
+ON cus.customer_id = ord.customer_id
+JOIN sales.Order_items oi
+ON ord.order_id = oi.order_id
+GROUP BY CONCAT(first_name, ' ', last_name)
+ORDER BY TotalSales DESC;
