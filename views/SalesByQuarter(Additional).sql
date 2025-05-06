b8afa770fe1a3a7731af0d8466eafae7578e4e13
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-06 10:50:24 +0500

    Update and rename PerformanceBySales(Additional).sql to SalesByQuarter(Additional).sql

---
+++ views/SalesByQuarter(Additional).sql
@@ -0,0 +1,11 @@
+CREATE VIEW vw_SalesByQuarter AS
+SELECT 
+	YEAR(order_date) Year,
+	'Q' + CAST(DATEPART(QUARTER, order_date) AS VARCHAR(1)) Quarter,
+	SUM((quantity*list_price)*(1-discount)) Sales
+FROM Sales.Customers cus
+JOIN sales.Orders ord
+ON cus.customer_id = ord.customer_id
+JOIN sales.Order_items oi
+ON ord.order_id = oi.order_id
+GROUP BY YEAR(order_date), DATEPART(QUARTER, order_date)
