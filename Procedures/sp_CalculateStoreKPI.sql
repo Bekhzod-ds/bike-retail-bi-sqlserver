e588c8025b41b47101e0265b858aed177a9a8830
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-07 22:56:52 +0500

    Create sp_CalculateStoreKPI.sql

---
+++ Procedures/sp_CalculateStoreKPI.sql
@@ -0,0 +1,26 @@
+create proc sp_CalculateStoreKPI @storeID int
+as
+with CTEMultipleVisits as(
+select
+	store_id,
+	Customer_id
+from sales.Orders
+where store_id = @storeID
+group by 
+	store_id,
+	customer_id
+having count(*) > 1
+),
+CTECountRentn as(
+select store_id,
+	round((cast(count(*) as float)/(select count(*) from sales.Orders where store_id = CTEMultipleVisits.store_id))*100, 2) Retention_rate
+from CTEMultipleVisits
+group by store_id
+)
+select sss.*, 
+	Retention_rate,
+	CountOfStaff
+from CTECountRentn cte
+join vw_StoreSalesSummary sss
+on cte.store_id = sss.store_id
+cross apply (select count(*) CountOfStaff from sales.Staff where store_id = sss.store_id) staff;
