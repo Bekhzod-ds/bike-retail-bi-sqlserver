42a9ba1493158f0cdce9a9e16b8ce041fada608f
Asadullo-17 <abduvohidovasadullo1707@gmail.com>
2025-05-07 21:08:15 +0500

    Update Orders&RevenuePerStaff.sql

--- views/Orders&RevenuePerStaff.sql
+++ views/Orders&RevenuePerStaff.sql
@@ -1,8 +1,9 @@
-create view vw_StaffPerformance as(
+create view vw_StaffPerformance as (
   select o.staff_id,
-		count(distinct o.order_id) as orders_per_staff,
-		sum(oi.list_price) as revenue_per_staff
-  from sales.orders o
-  join sales.order_items oi
-  on o.order_id=oi.order_id
-  group by o.staff_id)
+  		count(distinct o.order_id) as orders_per_staff,
+    sum(oi.quantity * oi.list_price * (1 - oi.discount)) as total_revenue
+from sales.Order_items oi
+join sales.Orders o 
+on oi.order_id = o.order_id
+group by o.staff_id
+ )
