beee90c67d62fae452767ab92ad466b2ed854204
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-11 17:57:48 +0500

    Update StoreSalesSummary.sql

--- views/StoreSalesSummary.sql
+++ views/StoreSalesSummary.sql
@@ -14,4 +14,5 @@ GROUP BY
 	store_id,
 	store_name;
 
---This view allows us to see the performance (Revenue, Number of orders, and AOV) of each of the company's stores
+--This view allows us to see the performance (Revenue, Number of orders, and AOV) of each of the company's stores.
+--AOV or Average Order Value is a crucial KPI indicator as it allows the company analyze customer spending behavior. This insight may help develop strategies to make customers purchase above that Average by setting a minimum cost for free shipping, or a bonus product, or a discount or sth else. In this case we have AOV of 4900, 4600 and 4700 for three stores store. We could offer free shipping or a discount if the order price is above 5000.
