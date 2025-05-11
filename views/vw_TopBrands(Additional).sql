09b59c1cea990939b13653be01b82c72ba1aca84
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-11 18:48:41 +0500

    Update vw_TopBrands(Additional).sql

--- views/vw_TopBrands(Additional).sql
+++ views/vw_TopBrands(Additional).sql
@@ -12,3 +12,4 @@ on p.brand_id = b.brand_id
 group by brand_name
 
 --This view allows us to see how popular each brand that supplies the company with products is. This may help the company identify strong brands whose products are best to sell and supply more of their products as customers are more likely ot buy them as opposed to products of those brands that are not so popular
+--with this data, we can identify products that do well, which should be promoted or products the company should probably discontinue.
