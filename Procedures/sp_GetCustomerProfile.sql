84b82b105bcf16dd62583f520bb7775f708d2002
Asadullo-17 <abduvohidovasadullo1707@gmail.com>
2025-05-13 22:51:50 +0500

    Update sp_GetCustomerProfile.sql

--- Procedures/sp_GetCustomerProfile.sql
+++ Procedures/sp_GetCustomerProfile.sql
@@ -2,39 +2,42 @@
 create procedure sp_GetCustomerProfile
 as
 begin
- ;
+ 
+    	with CTE_MostBought as(
+select
+  o.customer_id,
+  product_id,
+  sum(quantity) orderqty,
+  row_number() over (partition by customer_id order by sum(quantity) desc) rnk
+from sales.Orders o
+join sales.Order_items oi on o.order_id = oi.order_id
+group by o.customer_id, product_id 
+),
 
-    -- total spends and total orders per customer
-    select 
+ CTE_Totals as(
+
+select 
         o.customer_id,
         sum(oi.list_price * oi.quantity * (1 - oi.discount)) as total_spends,
         count(o.order_id) as total_orders
     from sales.Orders o
     join sales.Order_items oi on o.order_id = oi.order_id
+	
     group by o.customer_id
-	order by customer_id;
+	)
 
-    -- most bought items 
-    select 
-        customer_id,
-        product_id,
-        total_quantity
-    from (
-        select 
-            o.customer_id,
-            oi.product_id,
-            sum(oi.quantity) as total_quantity,
-            row_number() over (partition by o.customer_id order by sum(oi.quantity) desc) as rn
-        from sales.Orders o
-        join sales.Order_items oi on o.order_id = oi.order_id
-        group by o.customer_id, oi.product_id
-    ) ranked
-    where rn <= 5
-    order by customer_id, total_quantity desc;
 
+select
+  mb.customer_id,
+  t.total_spends,
+  t.total_orders,
+  mb.product_id as most_bought_product
+from CTE_MostBought mb
+join CTE_Totals t
+on mb.customer_id=t.customer_id
+
+where mb.rnk = 1
+    
 end;
 
-execute sp_GetCustomerProfile1
-
-
-
+execute sp_GetCustomerProfile
