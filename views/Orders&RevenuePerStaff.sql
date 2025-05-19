e7479375c5850341cacbf9815bba4c978a8ff187
Reinhardt <shsulaymonov2007@gmail.com>
2025-05-19 12:17:31 +0500

    Update Orders&RevenuePerStaff.sql

--- views/Orders&RevenuePerStaff.sql
+++ views/Orders&RevenuePerStaff.sql
@@ -1,9 +1,11 @@
 create view vw_StaffPerformance as (
-  select o.staff_id,
-  		count(distinct o.order_id) as orders_per_staff,
-    sum(oi.quantity * oi.list_price * (1 - oi.discount)) as total_revenue
+  select s.staff_id,
+  		isnull(count(distinct o.order_id), 0) as orders_per_staff,
+    isnull(sum(oi.quantity * oi.list_price * (1 - oi.discount)), 0) as total_revenue
 from sales.Order_items oi
 join sales.Orders o 
 on oi.order_id = o.order_id
-group by o.staff_id
+right join sales.Staff s
+on o.staff_id = s.staff_id
+group by s.staff_id
  )
